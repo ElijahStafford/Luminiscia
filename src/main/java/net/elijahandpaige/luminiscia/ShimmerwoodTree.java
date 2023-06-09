dd24197364fe77a9ce6dd19eb0dd948921c86625
ElijahStafford <es1mb05@limestone.on.ca>
2023-06-09 02:01:19 -0400

    spline root test

--- src/main/java/net/elijahandpaige/luminiscia/ShimmerwoodTree.java
+++ src/main/java/net/elijahandpaige/luminiscia/ShimmerwoodTree.java
@@ -8,10 +8,9 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
-import net.minecraft.util.math.random.Random;
+import net.minecraft.util.math.*;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
@@ -19,6 +18,7 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static net.elijahandpaige.luminiscia.Luminiscia.MOD_ID;
 
@@ -31,23 +31,53 @@ public class ShimmerwoodTree extends Feature<DefaultFeatureConfig> {
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
         StructureWorldAccess world = context.getWorld();
         BlockPos origin = context.getOrigin();
+        var currPos = new Vec3d(origin.getX(), origin.getY(), origin.getZ());
 
         var pos = new ArrayList<Vec3i>();
-        pos.add(new Vec3i(0,6,-4));
-        pos.add(new Vec3i(2,6,-2));
-        pos.add(new Vec3i(0,6,2));
-        pos.add(new Vec3i(1,8,7));
+        var rand = new Random();
+        var direction = new Vec3d(0, 0, 10);
+
+        for (int i = 0; i <= 5; i++) {
+            var rotation = new Vec2f(-rand.nextInt(40) - 40, rand.nextInt(360));
+
+            pos.add(new Vec3i(
+                    Math.round((float)currPos.x),
+                    Math.round((float)currPos.y),
+                    Math.round((float)currPos.z)
+            ));
+
+            currPos = rotateVector(direction, rotation, currPos);
+        }
 
         for (float i = 0; i <= 1; i += 1f / 100) {
             BlockPos position = new BlockPos(CatmullRomSpline.getPoint(pos, i));
-            position = position.add(origin);
             setBlockState(world, position, Blocks.REDSTONE_BLOCK.getDefaultState());
         }
 
         for (var position : pos) {
-            setBlockState(world, new BlockPos(position.add(origin)), Blocks.DIAMOND_BLOCK.getDefaultState());
+            setBlockState(world, new BlockPos(position), Blocks.DIAMOND_BLOCK.getDefaultState());
         }
 
         return true;
+    }
+
+    public Vec3d rotateVector(Vec3d vector, Vec2f rotation) {
+        return rotateVector(vector, rotation, new Vec3d(0,0,0));
+    }
+
+    public Vec3d rotateVector(Vec3d vector, Vec2f rotation, Vec3d anchor) {
+        float f = MathHelper.cos((rotation.y + 90.0F) * 0.017453292F);
+        float g = MathHelper.sin((rotation.y + 90.0F) * 0.017453292F);
+        float h = MathHelper.cos(-rotation.x * 0.017453292F);
+        float i = MathHelper.sin(-rotation.x * 0.017453292F);
+        float j = MathHelper.cos((-rotation.x + 90.0F) * 0.017453292F);
+        float k = MathHelper.sin((-rotation.x + 90.0F) * 0.017453292F);
+        Vec3d vec3d2 = new Vec3d((double)(f * h), (double)i, (double)(g * h));
+        Vec3d vec3d3 = new Vec3d((double)(f * j), (double)k, (double)(g * j));
+        Vec3d vec3d4 = vec3d2.crossProduct(vec3d3).multiply(-1.0);
+        double d = vec3d2.x * vector.z + vec3d3.x * vector.y + vec3d4.x * vector.x;
+        double e = vec3d2.y * vector.z + vec3d3.y * vector.y + vec3d4.y * vector.x;
+        double l = vec3d2.z * vector.z + vec3d3.z * vector.y + vec3d4.z * vector.x;
+        return new Vec3d(anchor.x + d, anchor.y + e, anchor.z + l);
     }
 }
